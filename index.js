a44362404382a12e0e25e2039c6188cc9029f994
Hingaz <hhinga04@gmail.com>
2023-09-29 16:20:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+
+
+main. remove();
+
+
+let newHeader = document.createElement('h1');
+
+
+ let victory = document.createTextNode('victory');
+ newHeader.appendChild(victory);
+
+ //let element2 = document.getElementById('div1');
+ //element2.appendChild(hingz);
+
+//const newHeader = document.createElement("h1");
+ 
+
